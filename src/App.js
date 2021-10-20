8b2136ac69435857472c585c3a80f019ff6e5b6c
carlos <carlos.wesley.jesus@gmail.com>
2021-10-19 22:21:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Cadastro from './Pages/Cadastro/Cadastro';
+import TarefaView from './Pages/TarefaView/TarefaView';
+import Edicao from './Pages/Edicao/Edicao'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header/>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/Cadastro" component={Cadastro} />
+          <Route path="/view/:id" component={TarefaView}/>
+          <Route path="/edit/:id" component={Edicao}/>
+        </Switch>
+      <Footer/>      
     </div>
   );
 }
---
+++ src/Components/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="w-100">
+            <div className="container">
+                <p className="text-center"> @Cbernardox </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
